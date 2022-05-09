f27cba20310048637462609bc950bee7dd5cb375
Nicklas <nicklas_tp@hotmail.com>
2022-05-09 16:45:44 +0200

    commit

--- src/routes/PageTwo.tsx
+++ src/routes/PageTwo.tsx
@@ -1,6 +1,23 @@
+import { useState } from "react"
+
 const PageTwo = () => {
+    const [name, setName] = useState('')
+
+    const submitHandler = (e: any) => {
+      setName(e.target.value)
+      e.preventDefault()
+    }
+
     return (
-      <div className="p-5">Page Two</div>
+      <>
+        <div className="p-5">
+          <h1>{name}</h1>
+          <form onSubmit={submitHandler}>
+            <input type="text" name="name" />
+            <button type="submit" value="submit">Submit</button> 
+          </form>
+        </div>
+      </>
     )
 }
 
