534a971a85fd84e8abc1bf662acc9f8112704f5b
Nicklas <nicklas_tp@hotmail.com>
2022-05-09 22:11:54 +0200

    commit

--- src/routes/PageTwo.tsx
+++ src/routes/PageTwo.tsx
@@ -3,18 +3,18 @@ import { useState } from "react"
 const PageTwo = () => {
     const [name, setName] = useState('')
 
-    const submitHandler = (e: any) => {
-      setName(e.target.value)
-      e.preventDefault()
+    const handleSubmit = (e: any) => {
+      e.preventDefault();
+      console.log(`Form submitted, ${name}`);    
     }
 
     return (
       <>
-        <div className="p-5">
-          <h1>{name}</h1>
-          <form onSubmit={submitHandler}>
-            <input type="text" name="name" />
-            <button type="submit" value="submit">Submit</button> 
+        <div className="p-5 bg-slate-500">
+          <h1 className="p-5 bg-red-900">{name}</h1>
+          <form className="p-5" onSubmit = {handleSubmit}>
+            <input className="m-4" onChange = {(e) => setName(e.target.value)} value={name}></input>
+            <button type='submit'>Click to submit</button>
           </form>
         </div>
       </>
